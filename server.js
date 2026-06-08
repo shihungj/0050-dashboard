7ec6682745e7ff2ba6c40f4f9b9aad10721994a0
Ivan Chang 張世鴻 <Ivan.Chang@tasc.com.tw>
2026-06-08 12:10:43 +0800

    remove api test page

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 import express from "express";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-// 提供 public 資料夾
 app.use(express.static("public"));
 
-
-// API測試
 app.get("/api/quote", async (req, res) => {
 
   try {
@@ -16,26 +13,9 @@ app.get("/api/quote", async (req, res) => {
       "https://query1.finance.yahoo.com/v8/finance/chart/0050.TW"
     );
 
-    if (!response.ok) {
-      throw new Error(
-        `Yahoo API Error: ${response.status}`
-      );
-    }
-
     const data = await response.json();
 
-    console.log(
-      "Yahoo Response:",
-      JSON.stringify(data, null, 2)
-    );
-
-    const result = data.chart?.result?.[0];
-
-    if (!result) {
-      throw new Error(
-        "Yahoo API returned no result"
-      );
-    }
+    const result = data.chart.result[0];
 
     res.json({
       symbol: "0050.TW",
@@ -46,7 +26,7 @@ app.get("/api/quote", async (req, res) => {
 
   } catch (error) {
 
-    console.error("API Error:", error);
+    console.error(error);
 
     res.status(500).json({
       error: error.message
@@ -56,9 +36,6 @@ app.get("/api/quote", async (req, res) => {
 
 });
 
-// 啟動 Server
 app.listen(PORT, () => {
-  console.log(
-    `Server running at http://localhost:${PORT}`
-  );
+  console.log(`Server running on ${PORT}`);
 });
